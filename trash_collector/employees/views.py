c9953931f4c7e91e9f89205057608ec7cf0a19eb
Samuel Guthrie <lottguthrie@yahoo.com>
2021-10-25 14:59:40 -0500

    Added admin, models, urls, and views to the employee portion of the project

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,61 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from datetime import date
+
+from.models import Employee
+from customers.models import Customer
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employees = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employees': logged_in_employees,
+            'today': today
+        }
+        return render(request, 'employee/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_from_form = request.POST.get('weekly_pickup')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')    
+
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employees = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employees': logged_in_employees
+        }
+        return render(request, 'employees/edit_profile.html', context)
